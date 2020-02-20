1a41b9f750ca33634c2c2cb848eadc63a105dbdc
Jordy Schifferstein <jordyschifferstein@hotmail.com>
2020-02-20 22:00:48 +0100

    push

--- greedy_j.py
+++ greedy_j.py
@@ -14,13 +14,17 @@ def jordy(data):
     books = {}
 
     for library in libraries_to_load:
-        books[library] = {}
+        book_scores = []
+        books[library] = []
 
         for book in data[library]['books']:
-            book[library][book] = scores[book]
+            book_scores.append(scores[book])
 
+        for i in range(len(book_scores)):
+            max_value = max(book_scores)
+            books[library].append(data[library]['books'].pop(book_scores.index(max_value)))
+            del book_scores[book_scores.index(max_value)]
 
-    
     for day in range(days):
         if day >= day_setup and len(libraries_to_load):
             if lib_load:
@@ -30,11 +34,13 @@ def jordy(data):
             day_setup = day + data[lib_load]['process']
 
         for library in libraries_setup:
+            books_to_send = set()
+
             try:
-                books_to_send = set(rnd.sample(data[library]['books'], data[library]['shipping']))
-            except ValueError:
-                books_to_send = set(data[library]['books'])
-            data[library]['books'] - books_to_send
+                for i in range(data[library]['shipping']):
+                    books_to_send.add(books[library].pop(0))
+            except IndexError:
+                pass
 
             if library in sent_books:
                 for book in books_to_send:
@@ -45,19 +51,26 @@ def jordy(data):
             for book in books_to_send:
                 books_sent.add(book)
 
-    with open('result.txt', 'w') as file:
-        file.write(f'{len(libraries_setup)}\n')
-
-        for library in libraries_setup:
-            file.write(f'{library} {len(sent_books[library])}\n')
-
-            for book in sent_books[library]:
-                file.write(f'{str(book)} ')
-            
-            file.write('\n')
-    
     for book in books_sent:
         score.append(scores[book])
 
-    print(sum(score))
+    dict_write = {
+        'amount_libraries_setup': len(libraries_setup),
+        'libraries_setup': libraries_setup,
+        'sent_books': sent_books,
+        'score': sum(score)
+    }
+
+    with open('result.txt', 'w') as file:
+        file.write(f"{dict_write['amount_libraries_setup']}\n")
+
+        for library in dict_write['libraries_setup']:
+            file.write(f"{library} {len(dict_write['sent_books'][library])}\n")
+
+            for book in dict_write['sent_books'][library]:
+                file.write(f'{str(book)} ')
+            
+            file.write('\n')
+
+    print(dict_write['score'])
     return sum(score)
