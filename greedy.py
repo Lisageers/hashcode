02dc151e6e04e11ffa2b41fabd76e8709f34113a
Emma Sarah Caarls <emma.caarls@student.uva.nl>
2020-02-20 21:03:43 +0100

    greedy met fout erin

--- greedy.py
+++ greedy.py
@@ -4,6 +4,33 @@ def greedy(data):
 	process_times = []
 
 	for x in range(number_libraries):
-		process_times.append((data[str(x)]["process"], x))
+		process_times.append((data[str(x)]["process"], data[str(x)]))
 	
-	process_times.sort()
+	process_times.sort(reverse=True)
+	
+	day_setup = 0
+	scanned_books = set()
+	libs_setup = []
+	libs_scanned = []
+
+	for day in range(data["days"]):
+		if day >= day_setup and len(process_times):
+			day_setup += process_times[-1][0]
+			libs_setup.append(process_times[-1][1])
+			process_times.pop()
+
+		for library in libs_setup:
+			libs_books = library["books"]
+			libs_ship = int(library["shipping"])
+			for book in libs_books[-libs_ship:]:
+				scanned_books.add(int(book))
+			del library["books"][-libs_ship:]
+
+	total_score = 0
+
+	scores = data["scores"]
+
+	for book in scanned_books:
+		total_score += scores[book]
+
+	return total_score
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from random_alg import random_alg
 from sys import argv
 
 if len(argv) < 3:
-    print('typ an argument')
-    exit()
+	print('type an argument')
+	exit()
 
 algorithm = argv[2]
 filename = argv[1]
@@ -13,6 +13,7 @@ filename = argv[1]
 data = load(filename)
 
 if algorithm == 'rand':
-    random_alg(data)
+	random_alg(data)
 elif algorithm == 'greedy':
-    greedy(data)
+	score = greedy(data)
+	print(score)
