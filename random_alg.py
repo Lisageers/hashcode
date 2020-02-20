b39ad22fe93a8d0ea1125330bc76e6b6726a3d72
Jordy Schifferstein <jordyschifferstein@hotmail.com>
2020-02-20 20:07:16 +0100

    random

--- random_alg.py
+++ random_alg.py
@@ -2,7 +2,28 @@ import random as rnd
 
 def random_alg(data):
     days = data['days']
+    libraries_to_load = list(data.keys())[4:]
+    libraries_setup = set()
+    day_setup = 0
+    lib_load = None
+    score = set()
+    scores = data['scores']
     
-    libraries = list(data.keys())[4:]
-    first_lib = rnd.choice(libraries)
-    print(first_lib)
+    for day in range(days):
+        if day >= day_setup and len(libraries_to_load):
+            if lib_load:
+                libraries_setup.add(lib_load)
+
+            lib_load = rnd.choice(libraries_to_load)
+            del libraries_to_load[libraries_to_load.index(lib_load)]
+            day_setup = day + data[lib_load]['process']
+
+        for library in libraries_setup:
+            books_to_send = set(rnd.sample(data[library]['books'], data[library]['shipping']))
+            data[library]['books'] - books_to_send
+
+            for book in books_to_send:
+                score.add(scores[book])
+
+    print(sum(score))
+
