a9979c7218cdbd981b127ae385a5fdc67dee36c9
Emma Sarah Caarls <emma.caarls@student.uva.nl>
2020-02-20 19:28:51 +0100

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+from load import load
+from greedy import greedy
+
+filename = input("enter filename: ")
+
+data = load(filename)
+
+# run er een random agoritme op
+
+# run er een greedy algoritme op
